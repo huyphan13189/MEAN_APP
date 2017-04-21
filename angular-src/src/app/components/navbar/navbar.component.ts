df8c034d8eeb4a32cffb8cd83f86990ca7cafb8a
Huy Phan <yoyo13189@gmail.com>
2017-04-21 23:19:09 +0700

    first commit

---
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
+import {FlashMessagesService} from 'angular2-flash-messages';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private flasMassageService: FlashMessagesService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onLogoutClick(){
+    this.authService.logout();
+    this.flasMassageService.show('You are logged out', {cssClass:'alert-success', timeout: 10000});
+    this.router.navigate(['/login']);
+  }
+
+  display(){
+    return false
+  }
+
+}
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://jack:123456@ds041561.mlab.com:41561/meanauth',
+    secret: 'yoursecret'
+}
